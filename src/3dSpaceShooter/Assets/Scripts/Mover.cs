54916d2d1299ee41bbefad820e6c0ee56ffa3206
rafura <afura.rs@gmail.com>
2019-04-29 20:34:12 -0300

    adcionando arquivos ao projeto

---
+++ src/3dSpaceShooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = transform.forward * speed;
+    }
+}
