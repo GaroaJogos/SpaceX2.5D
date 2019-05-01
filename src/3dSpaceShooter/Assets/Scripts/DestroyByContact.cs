e2541d667e6cc0f493426d7925c3b2f6af36e840
rafura <afura.rs@gmail.com>
2019-05-01 19:41:08 -0300

    adição dos asteroids

---
+++ src/3dSpaceShooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explotion;
+    public GameObject playerExplotion;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+            return;
+
+        if (other.tag == "Player")
+            Instantiate(playerExplotion, other.transform.position, other.transform.rotation);
+
+        Instantiate(explotion, transform.position, transform.rotation);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
