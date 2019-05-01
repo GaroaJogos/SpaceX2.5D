e52dd7ba3e3d12c0c8d9e448975c191658634661
rafura <afura.rs@gmail.com>
2019-05-01 12:25:45 -0300

    arquivos restantes

---
+++ src/3dSpaceShooter/Assets/Scripts/RandonRotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandonRotator : MonoBehaviour
+{
+    public float tumble;
+    private Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+
+}
