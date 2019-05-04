361ad9d63f2b68c55ad1533967917656e52bce18
rafura <afura.rs@gmail.com>
2019-05-04 12:53:06 -0300

    Adição do audio

--- src/3dSpaceShooter/Assets/Scripts/PlayerController.cs
+++ src/3dSpaceShooter/Assets/Scripts/PlayerController.cs
@@ -20,6 +20,12 @@ public class PlayerController : MonoBehaviour
     public float fireRate;
 
     private float nextFire;
+    AudioSource sound;
+
+    private void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
 
     void Update()
     {
@@ -27,6 +33,7 @@ public class PlayerController : MonoBehaviour
         {
             nextFire = Time.time + fireRate;
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            sound.Play();
         }
     }
 
